e2bc6a72fbd8f0d9a9700537276e1517b5594b3e
Praveen Agrawal <pr.agrawal@reply.com>
2019-09-29 16:20:26 +0100

    MF test: incrementally reverses a string

---
+++ src/com/mf/test/Main.java
@@ -0,0 +1,10 @@
+package com.mf.test;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Incremental reverse of 'String' is: " + MatchesTest.myFunction("String"));
+        System.out.println("Incremental reverse of 'Praveen' is: " + MatchesTest.myFunction("Praveen"));
+    }
+}
---
+++ src/com/mf/test/MatchesTest.java
@@ -0,0 +1,63 @@
+package com.mf.test;
+
+public class MatchesTest
+{
+    /**
+     * Assuming that intermediate string patterns stated in the problem are not required.
+     * Hence this implementation will not be able to generate those intermediate string patterns.
+     * If intermediate string patterns are required to print, then use method 'myFunction2(String str)'.
+     *
+     * @param str input string
+     * @return incrementally reverses input string for each character in the string.
+     */
+    public static String myFunction(String str)
+    {
+        if(str == null || str.isEmpty()){
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0,j=str.length()-1; i< (str.length()+1)/2 ; i++,j--){
+
+            if(i==j)
+            {
+                sb.append(str.charAt(i));
+                break;
+            }
+            sb.append(str.charAt(j));
+            sb.append(str.charAt(i));
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Assuming that intermediate string patterns stated in the problem are important and required to generate.
+     * Hence this implementation will generate all the intermediate string patterns and finally the desired output string.
+     * Also look at method 'myFunction(String str)'. It is more optimised but does't generate the intermediate string patterns.
+     *
+     * @param str input string
+     * @return incrementally reverses input string for each character in the string.
+     */
+    public static String myFunction2(String str)
+    {
+        if(str == null || str.isEmpty()){
+            return "";
+        }
+
+        StringBuilder sb = new StringBuilder(str);
+        sb.reverse();
+
+        StringBuilder reverse = new StringBuilder();
+
+        for(int i=0; i < str.length() ; i++)
+        {
+            System.out.println(reverse.toString()+sb);
+            reverse.append(sb.charAt(0));
+            sb.delete(0,1);
+            sb.reverse();
+        }
+        return reverse.toString();
+    }
+
+
+}
